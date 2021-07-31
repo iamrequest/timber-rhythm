83350943fb8d55470f8f3072488a21b57ad7c220
request <iamrequest@protonmail.com>
2021-07-31 17:01:43 -0400

    More UI work
    - Added slider to show loop progress
    - Added play/stop icons to play/stop buttons
    - Docked UI to player hand
    - Added (buggy) animation to fade the UI menu in and out, depending on view angle

---
+++ Assets/Scripts/UI/LoopProgressListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class LoopProgressListener : MonoBehaviour {
+    private Slider loopSlider;
+    private Image fillImage;
+    public Color playColor, recordingColor;
+
+    private void Awake() {
+        loopSlider = GetComponent<Slider>();
+        fillImage = loopSlider.fillRect.GetComponent<Image>();
+    }
+
+    private void Update() {
+        loopSlider.value = LoopMachine.Instance.t;
+
+        if (LoopMachine.Instance.isRecording) {
+            fillImage.color = recordingColor;
+        } else {
+            fillImage.color = playColor;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIDockingManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDockingManager : MonoBehaviour {
+    public Camera cam;
+    private Animator animator;
+    private int isSettingsMenuOpenHash;
+    public float viewAngle;
+
+    public Transform dockingTransform;
+
+    [Range(0f, 1f)]
+    public float lerpSpeed = 0.4f;
+    [Range(0f, 180f)]
+    public float settingsOpenAngle = 30f, settingsCloseAngle = 45f;
+
+    private void Awake() {
+        animator = GetComponentInChildren<Animator>();
+        isSettingsMenuOpenHash = Animator.StringToHash("isSettingsMenuOpen");
+    }
+
+    private void FixedUpdate() {
+        transform.position = Vector3.Lerp(transform.position, dockingTransform.position, lerpSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, dockingTransform.rotation, lerpSpeed);
+
+        viewAngle = Vector3.Angle(cam.transform.forward, -transform.forward);
+        if (animator.GetBool(isSettingsMenuOpenHash)) {
+            if (viewAngle > settingsCloseAngle) {
+                animator.SetBool(isSettingsMenuOpenHash, false);
+            }
+        } else {
+            if (viewAngle < settingsOpenAngle) {
+                animator.SetBool(isSettingsMenuOpenHash, true);
+            }
+        }
+    }
+}
