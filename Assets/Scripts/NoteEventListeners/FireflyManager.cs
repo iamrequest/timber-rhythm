3fb1036cdf5a467b6ebf2a055d1c8fd6e0ac451b
request <iamrequest@protonmail.com>
2021-08-01 18:10:05 -0400

    Added fireflies that spawn when the snare is hit

---
+++ Assets/Scripts/NoteEventListeners/FireflyManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireflyManager : ImpactNoteEventListener {
+    private List<Fireflies> fireFlySpots;
+
+    private void Awake() {
+        fireFlySpots = new List<Fireflies>();
+        GetComponentsInChildren(fireFlySpots);
+    }
+
+    public override void RaiseEvent(ImpactNote impactNote) {
+        fireFlySpots[Random.Range(0, fireFlySpots.Count)].OnNoteEvent();
+    }
+}
