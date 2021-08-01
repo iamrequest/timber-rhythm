e8c514a2fb22e0de51925d7cb84a49a4eb46ca68
request <iamrequest@protonmail.com>
2021-08-01 09:49:54 -0400

    Added snare drum
    - Wasn't able to easily make a separate child gameobject that would listen for collisions, so the whole drum can make snare noises

---
+++ Assets/Scripts/Interactables/FollowTransform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Cheap hack to semi-parent a noise source to a drum face. Can't have multiple child rigidbodies, so this will have to do
+/// </summary>
+public class FollowTransform : MonoBehaviour {
+    public Transform target;
+    private Rigidbody rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        rb.position = target.position;
+        rb.rotation = target.rotation;
+    }
+}
--- Assets/Scripts/Music/ImpactNoteSource.cs
+++ Assets/Scripts/Music/ImpactNoteSource.cs
@@ -1,20 +1,35 @@
-﻿using System.Collections;
+﻿using HurricaneVR.Framework.Core;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(ImpactNote))]
 public class ImpactNoteSource : NoteSource {
+    private HVRGrabbable grabbable; 
+
     [HideInInspector]
     public ImpactNote note;
     public ImpactNoteSoundLibrary soundLibrary;
 
+    [Range(0f, 1f)]
+    public float cooldown;
+    private float elapsedCooldown;
+
     private void Awake() {
+        grabbable = GetComponent<HVRGrabbable>();
         note = GetComponent<ImpactNote>();
         note.noteSource = this;
     }
 
+    private void Update() {
+        elapsedCooldown = Mathf.Clamp(elapsedCooldown + Time.deltaTime, 0f, cooldown);
+    }
+
     private void OnCollisionEnter(Collision collision) {
         if (!DoesCollisonPlayNote(collision.collider.gameObject.layer)) return;
+        if (grabbable.IsBeingHeld) return;
+        if (elapsedCooldown < cooldown) return;
+        elapsedCooldown = 0f;
 
         // Pick a random sound from this instrument's sound library
         note.audioClip = soundLibrary.sounds[Random.Range(0, soundLibrary.sounds.Count)];
