92d3394bb000a14eb65a37aef703a5490ac2c362
request <iamrequest@protonmail.com>
2021-08-01 18:58:35 -0400

    Added falling leaves on crash cymbal

---
+++ Assets/Scripts/NoteEventListeners/OneShotVFXEventListener.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+/// <summary>
+/// Calls vfx.Play() for a random VisualEffect, when an impact event happens
+/// </summary>
+public class OneShotVFXEventListener : ImpactNoteEventListener {
+    private List<VisualEffect> vfxList;
+
+    private void Awake() {
+        vfxList = new List<VisualEffect>();
+        GetComponentsInChildren(vfxList);
+    }
+
+    public override void RaiseEvent(ImpactNote impactNote) {
+        base.RaiseEvent(impactNote);
+        vfxList[Random.Range(0, vfxList.Count)].Play();
+    }
+}
