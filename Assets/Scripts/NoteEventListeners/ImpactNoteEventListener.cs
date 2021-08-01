cb38b4af03c0d4d40a55526d9313a67dad695a9a
request <iamrequest@protonmail.com>
2021-08-01 18:40:29 -0400

    Tweaked fireflies, pond now ripples on kick drum

--- Assets/Scripts/NoteEventListeners/ImpactNoteEventListener.cs
+++ Assets/Scripts/NoteEventListeners/ImpactNoteEventListener.cs
@@ -18,10 +18,21 @@ public class ImpactNoteEventListener : MonoBehaviour {
         eventChannel.onNotePlayed -= RaiseEvent;
     }
 
+    /// <summary>
+    /// Called on every event
+    /// </summary>
+    /// <param name="impactNote"></param>
     public virtual void RaiseEvent(ImpactNote impactNote) {
         // Delegate the event to a local UnityEvent, if we pass the random chance check
         if (eventResponseChance > Random.Range(0f, 1f)) {
             onEventPlayed.Invoke();
+            ProcessEvent(impactNote);
         }
     }
+
+    /// <summary>
+    /// Called when the RNG check succeeds
+    /// </summary>
+    /// <param name="impactNote"></param>
+    public virtual void ProcessEvent(ImpactNote impactNote) { }
 }
---
+++ Assets/Scripts/NoteEventListeners/PondEventListener.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Freya;
+
+public class PondEventListener : ImpactNoteEventListener {
+    [Range(0f, 1f)]
+    public float maxRipple;
+    [Range(0f, 1f)]
+    public float releaseDuration;
+    private float timeSinceLastNote;
+    private Renderer m_renderer;
+    private float rippleStrength;
+
+    private void Awake() {
+        m_renderer = GetComponent<Renderer>();
+        timeSinceLastNote = releaseDuration;
+    }
+    private void Update() {
+        timeSinceLastNote += Time.deltaTime;
+        rippleStrength = Mathfs.RemapClamped(0f, releaseDuration, maxRipple, 0f, timeSinceLastNote);
+        m_renderer.material.SetFloat("_rippleStrength", rippleStrength);
+    }
+    public override void ProcessEvent(ImpactNote impactNote) {
+        timeSinceLastNote = 0f;
+    }
+}
